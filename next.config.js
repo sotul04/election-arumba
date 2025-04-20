b210ee775ed5539662720e164904e37d0ee32fd9
sotul04 <109208105+sotul04@users.noreply.github.com>
2025-04-20 15:39:23 +0700

    remove workflow

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    output: 'standalone',
     images: {
         dangerouslyAllowSVG: true,
         minimumCacheTTL: 3600,
